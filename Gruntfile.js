e84d3252f147fc312e072d083b2b66ee501bedde
Owen McAteer <o.mcateer@catchdigital.com>
2016-09-06 18:58:14 +0200

    Project management workflow.

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function (grunt) {
+  'use strict';
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: ['src/Intro.js', 'src/core/test.js', 'src/Outro.js'],
+        //src: ['src/Intro.js', 'src/core/*.js', 'src/Outro.js'],
+        dest: 'dist/microcontrol.js'
+      }
+    },
+    jshint: {
+      // esversion: 6.
+      options: {
+        node: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        eqnull: true,
+        boss: true
+      },
+      all: {
+        src: ['gruntfile.js', 'src/**/*.js', 'tests/**/*.js']
+      }
+    },
+    nodeunit: {
+      files: ['tests/**/*.js']
+    },
+    watch: {
+      all: {
+        files: ['gruntfile.js', 'src/**/*.js', 'src/**/*.jsx', 'tests/**/*.js'],
+        tasks: ['jshint:all', 'concat']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('test', ['jshint', 'nodeunit']);
+  grunt.registerTask('build', ['jshint', 'concat']);
+  grunt.registerTask('default', ['build', 'watch']);
+};
